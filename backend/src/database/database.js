07a1f8adda6f88eb7572bf2e8e0c4d1bebf184e1
dannymg <dannymg2002@gmail.com>
2023-01-10 14:46:36 -0500

    Agregando Dockerfiles

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -3,7 +3,9 @@ import Sequelize from "sequelize";
 const database = process.env.DB_NAME || "wearyou_db";
 const username = process.env.DB_USERNAME || "wearyou_user";
 const password = process.env.DB_PASSWORD || "";
+const host = process.env.DB_HOST || "localhost";
+
 export const sequelize = new Sequelize(database, username, password, {
-  host: "localhost",
+  host: host,
   dialect: "postgres",
 });
